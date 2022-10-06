3545c185915deb27c7cc5cfb779ba64fefeb1f4a
luis-z <luis.zumarraga@solera.com>
2022-10-06 11:50:33 +0200

    added react .env

--- src/components/signup.js
+++ src/components/signup.js
@@ -6,36 +6,37 @@ import { BrowserRouter, Route, Link, useNavigate } from "react-router-dom";
 
 
 export default function Signup() {
-
-
+  
   const navigate = useNavigate();
+
   function navBack() {
     navigate(process.env.PUBLIC_URL + "/")
   }
 
-  function createUser(){
-
+  function createUser() {
+    //process.env.REACT_APP_URL
   }
 
   return (
     <div>
-    <Button variant="contained" onClick={navBack} className='mar-5'>Go back</Button>
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { padding: "50px 20px", width: 300, margin: "20px auto" },
-      }} noValidate autoComplete="off" >
+      <Button variant="contained" onClick={navBack} className='mar-5'>Go back</Button>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { padding: "50px 20px", width: 300, margin: "20px auto" },
+        }} noValidate autoComplete="off" >
 
-      <div>
-        <h4>Registration form</h4>
-        <TextField id="outlined-basic" label="Name" variant="outlined" fullWidth className='mar-5'/>
-        <TextField id="outlined-basic" label="Email Address" variant="outlined" fullWidth className='mar-5'/>
-        <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth className='mar-5'/>
-        <TextField id="outlined-basic" label="Confim Password" variant="outlined" fullWidth className='mar-5'/>
-      </div>
-      <div className='frontpageButtons'>
-        <Button variant="outlined" onClick={createUser}>sign up</Button>
-      </div>
-    </Box></div>
+        <div>
+          <h4>Registration form</h4>
+          <TextField id="outlined-basic" label="Name" variant="outlined" fullWidth className='mar-5' />
+          <TextField id="outlined-basic" label="Email Address" variant="outlined" fullWidth className='mar-5' />
+          <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth className='mar-5' />
+          <TextField id="outlined-basic" label="Confim Password" variant="outlined" fullWidth className='mar-5' />
+        </div>
+        <div className='frontpageButtons'>
+          <Button variant="outlined" onClick={createUser}>Sign up</Button>
+        </div>
+      </Box>
+    </div>
   );
 }
